5b19c42c1ff0a122c3bb12903cba08f85c43ee0a
fransantosrod <franciscojavier_95@hotmail.com>
2024-08-23 14:37:23 +0200

    Create testPy.py

---
+++ testPy.py
@@ -0,0 +1,12 @@
+import requests
+
+
+myPassword = "this_is_my_password"
+myToken = "this_is_my_token"
+
+
+print ("This is my new password:")
+print (myPassword)
+
+print ("New token to connect:")
+print (myToken)
